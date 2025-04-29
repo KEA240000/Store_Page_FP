5e90e334143e0e993cc49a9f56a16a17b1a1359f
KEA240000 <kea240000@utdallas.edu>
2025-04-29 13:52:16 -0500

    Added elif statements to call upon corresponding functions in main function based on user input.

--- Code and Images/Main.py
+++ Code and Images/Main.py
@@ -13,6 +13,7 @@ def init_webpage_size():
 def gradient_bg(screen_size):
     """Creates a gradient background, please reference README file to see how to choose colors!"""
 
+
 def main():
     """Main function that calls functions that initilize the desired image/window size from user choice of 3 options.
        Standard Instagram Post (1080 x 1080), Standard Phone size (1080 x 1920), Standard Webpage (1920 x 1080) """
@@ -20,5 +21,13 @@ def main():
     pygame.display.set_caption("Shop Product Page")
     size_choice = input("Choose a size: Instagram = 1, Phone = 2, Webpage Size = 3: ")
     # Create functions to be called here that initialize the desired image/window size from user choice of size.
+    if size_choice == "1":
+        def init_instagram_post():
+    elif size_choice == "2":
+        def init_phone_size():
+    elif size_choice == "3":
+        def init_webpage_size():
+    else:
+        print("Please choose a valid option! Number input between 1-3 only.")
 if __name__ == "__main__":
     main()
