b654b4912d94b7105b1370c736dc54717ffa31b2
KEA240000 <kea240000@utdallas.edu>
2025-05-06 15:35:58 -0500

    After some downtime, I hopefully fixed the bug that crashes the entire computer. Also attempted to fix the flickering issues...

--- src/Main.py
+++ src/Main.py
@@ -1,50 +1,38 @@
 import pygame
 
-class Image1():
-    # Opens the placeholder image and sets the size and position.
-    def __init__(self, pos =(0,0), size=(100, 100)):
-        self.rect = pygame.Rect(pos, size)
-        self.image = self.load_image()
-        self.color = pygame.Color('white')
-    def load_image(self):
-        self.image = pygame.image.load("src/placeholder.png")
-        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
-        return self.image
+def image_1(pos, screen):
+    image = pygame.image.load("ApplePlaceholder.png")
+    image = pygame.transform.scale(image, (screen.get_width(), screen.get_height()))
+    screen.blit(image, pos)
+    pygame.display.update()
+    
+
     
         
 
 class Image2():
-    def __init__(self, pos =(100,100), size=(100, 100)):
-      self.rect = pygame.Rect(pos, size)
-      self.color = pygame.Color('white')
-
-    def update_surface(self):
-        surf = pygame.Surface((self.rect.width, self.rect.height))
-        surf.fill(self.color)
-        return surf
-    def draw(self, screen):
-        surf = self.update_surface()
-        screen.blit(surf, self.rect.topleft)
-        pygame.draw.rect(screen, self.color, self.rect, 2)
+    def __init__(self, pos, size=(50, 50)):
+        self.rect = pygame.Rect(pos, size)
+        self.image = self.load_image()
+        self.color = pygame.Color('white')
+    def load_image(self):
+        self.image = pygame.image.load("ApplePlaceholder.png")
+        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+        return self.image
 
 class Image3():
-    def __init__(self, pos =(200,200), size=(100, 100)):
-      self.rect = pygame.Rect(pos, size)
-      self.color = pygame.Color('white')
-
-    def update_surface(self):
-        surf = pygame.Surface((self.rect.width, self.rect.height))
-        surf.fill(self.color)
-        return surf
-    def draw(self, screen):
-        surf = self.update_surface()
-        screen.blit(surf, self.rect.topleft)
-        pygame.draw.rect(screen, self.color, self.rect, 2)
+    def __init__(self, pos, size=(50, 50)):
+        self.rect = pygame.Rect(pos, size)
+        self.image = self.load_image()
+        self.color = pygame.Color('white')
+    def load_image(self):
+        self.image = pygame.image.load("ApplePlaceholder.png")
+        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+        return self.image
 
 def init_instagram_post():
     screen_size = (540, 540)
     screen = pygame.display.set_mode(screen_size)
-    screen.fill((0, 0, 0))
     return screen
     
 
@@ -52,14 +40,12 @@ def init_instagram_post():
 def init_phone_size():
     screen_size = (540, 960)
     screen = pygame.display.set_mode(screen_size)
-    screen.fill((0, 0, 0))
     return screen
     
 
 def init_webpage_size():
     screen_size = (960, 540)
     screen = pygame.display.set_mode(screen_size)
-    screen.fill((0, 0, 0))
     return screen
     
 def gradient_bg(screen, color1, color2, target):
@@ -68,7 +54,7 @@ def gradient_bg(screen, color1, color2, target):
     pygame.draw.line(color_rect, color2, (0, 1), (1, 1))
     color_rect = pygame.transform.smoothscale(color_rect, (target.width, target.height))
     screen.blit(color_rect, target)
-
+    pygame.display.update(target)
 
 
 
@@ -89,34 +75,32 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
      # Create functions to be called here that initialize the desired image/window size from user choice of size.
         if size_choice == "1":
             screen = init_instagram_post()
             #THIS CODE HERE DETERMINES GRADIENT COLORS
-            gradient_bg(screen, color1, color2, screen.get_rect())
-            Image1.draw(screen)
-            Image2.draw(screen)
-            Image3.draw(screen)
+            #gradient_bg(screen, color1, color2, screen.get_rect())
+            image_1((0, 0), screen)
             pygame.display.flip()
         elif size_choice == "2":
             screen = init_phone_size()
             #THIS CODE HERE DETERMINES GRADIENT COLORS
             gradient_bg(screen, color1, color2, screen.get_rect())
-            Image1.draw(screen)
-            Image2.draw(screen)
-            Image3.draw(screen)
+            Image1((0, 0))
+            Image2((100, 100))
+            Image3((200, 200))
             pygame.display.flip()
         elif size_choice == "3":
             screen = init_webpage_size()
             #THIS CODE HERE DETERMINES GRADIENT COLORS
             gradient_bg(screen, color1, color2, screen.get_rect())
-            Image1.draw(screen)
-            Image2.draw(screen)
-            Image3.draw(screen)
+            Image1((0, 0))
+            Image2((100, 100))
+            Image3((200, 200))
             pygame.display.flip()
         else:
             print("Please choose a valid option! Number input between 1-3 only.")
-
     pygame.quit()
 
 
