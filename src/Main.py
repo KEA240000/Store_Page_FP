61a5fd302551fb14d4f70eabfdd58e75ff9a3d61
KEA240000 <kea240000@utdallas.edu>
2025-05-09 15:28:13 -0500

    renamed some object classes, and added a new one to dictate the prices for independent text size.

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.font.init()
-FONT = pygame.font.SysFont('Courier New', 40)
+FONT = pygame.font.SysFont('Courier New', 30)
 
 def init_instagram_post():
     screen_size = (540, 540)
@@ -40,7 +40,7 @@ class Logo():
     def draw(self, surface):
         self.surface.set_alpha(self.alpha)
         surface.blit(self.surface, self.pos)
-        image = pygame.image.load("ApplePlaceholder.png")
+        image = pygame.image.load("ShopSticker.png")
         image = pygame.transform.scale(image, self.size)
         surface.blit(image, self.pos)
 
@@ -50,7 +50,7 @@ class Image1():
         self.size = size
         self.color = pygame.Color('white')
         self.rect = pygame.Rect(self.pos, self.size)
-        image = pygame.image.load("ShopSticker.png")
+        image = pygame.image.load("ApplePlaceholder.png")
         image = pygame.transform.scale(image, self.size)
         self.surface = self.update_surface()
         self.alpha = 255
@@ -75,7 +75,7 @@ class Image2():
         self.size = size
         self.color = pygame.Color('white')
         self.rect = pygame.Rect(self.pos, self.size)
-        image = pygame.image.load("ShopSticker.png")
+        image = pygame.image.load("ApplePlaceholder.png")
         image = pygame.transform.scale(image, self.size)
         self.surface = self.update_surface()
         self.alpha = 255
@@ -99,7 +99,7 @@ class Image3():
         self.size = size
         self.color = pygame.Color('white')
         self.rect = pygame.Rect(self.pos, self.size)
-        image = pygame.image.load("ShopSticker.png")
+        image = pygame.image.load("ApplePlaceholder.png")
         image = pygame.transform.scale(image, self.size)
         self.surface = self.update_surface()
         self.alpha = 255
@@ -112,22 +112,24 @@ class Image3():
     def draw(self, surface):
         self.surface.set_alpha(self.alpha)
         surface.blit(self.surface, self.pos)
-        image = pygame.image.load("ShopSticker.png")
+        image = pygame.image.load("ApplePlaceholder.png")
         image = pygame.transform.scale(image, self.size)
         surface.blit(image, self.pos)
 
     
-class Textbox():
+class ShopNameTextbox():
     # CHANGE TEXT PARAMETER IN __init__() TO SHOP NAME
-    def __init__(self, pos, size, text="SHOP NAME"):
+    def __init__(self, size, pos, input_text):
+        FONT = pygame.font.SysFont('Courier New', 30)
         self.rect = pygame.Rect(pos, size)
-        self.text = text
+        self.text = input_text
         self.color = pygame.Color('black')
-        self.txt_surface = FONT.render(text, True, pygame.Color('white'))  
+        self.text = FONT.render(self.text, True, pygame.Color('white')) 
+        self.text_rect = self.text.get_rect(center=(self.rect.width // 2, self.rect.height // 2)) 
 
     def update(self):
         #resize text box in case diaglogue is too large.
-        width = max(self.txt_surface.get_width()+10)
+        width = max(self.txt_surface.get_width() - 10)
         self.rect.width = width
     
     def update_surface(self):
@@ -138,8 +140,32 @@ class Textbox():
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect, 50)  
         pygame.draw.rect(screen, 'white', self.rect, 5)  
-        screen.blit(self.txt_surface, (self.rect.x + 20, self.rect.y + 30))
+        screen.blit(self.text, self.text_rect)
 
+class PriceTextbox():
+    # CHANGE TEXT PARAMETER IN __init__() TO SHOP NAME
+    def __init__(self, size, pos, input_text):
+        FONT = pygame.font.SysFont('Courier New', 10)
+        self.rect = pygame.Rect(pos, size)
+        self.text = input_text
+        self.color = pygame.Color('black')
+        self.text = FONT.render(self.text, True, pygame.Color('white')) 
+        self.text_rect = self.text.get_rect(center=(self.rect.width // 2, self.rect.height // 2)) 
+
+    def update(self):
+        #resize text box in case diaglogue is too large.
+        width = max(self.txt_surface.get_width() - 10)
+        self.rect.width = width
+    
+    def update_surface(self):
+        surf = pygame.Surface((self.rect.width, self.rect.height))
+        surf.fill(self.color)
+        return surf
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect, 50)  
+        pygame.draw.rect(screen, 'white', self.rect, 5)  
+        screen.blit(self.text, self.text_rect)
 #class BackgroundImage():
     
 
@@ -152,23 +178,33 @@ def main():
     pygame.display.set_caption("Shop Product Page")
 
     screen = None
+    shop_name = input("What's the name of your shop? ")
+    price1_input = input("Enter the price of the first product: ")
+    price2_input = input("Enter the price of the second product: ")
+    price3_input = input("Enter the price of the third product: ")
     size_choice = input("Choose a size: Instagram = 1, Phone = 2, Webpage Size = 3: ")
     #THESE VARIABLES ARE FOT GRADIENT BACKGROUND COLORS
     if size_choice == "1":
         # instantiate screen, textbox, and logo
         screen = init_instagram_post()
-        textbox = Textbox((20, 0), (500, 100)) 
-        logo = Logo((100, 100), (0, 100)) 
-        image1 = Image1((50, 50),(0, 300))
-        image2 = Image2((50, 50),(100, 300))
-        image3 = Image3((50, 50),(200, 300)) 
+        textbox = ShopNameTextbox((500, 100), (20, 0), shop_name) 
+        logo = Logo((100, 100), (225, 100)) 
+        image1 = Image1((75, 75),(85, 250))
+        price1 = PriceTextbox((75, 50), (85, 350), price1_input)
+        image2 = Image2((75, 75),(235, 250))
+        price2 = PriceTextbox((75, 50), (235, 350), price2_input)
+        image3 = Image3((75, 75),(385, 250)) 
+        price3 = PriceTextbox((75, 50), (385, 350), price3_input)
     elif size_choice == "2":
-        screen = init_phone_size()
-        textbox = Textbox((20, 0))
-        logo = Logo((0, 100)) 
-        image1 = Image1((0, 100))
-        image2 = Image2((0, 200))
-        image3 = Image3((0, 300))
+        screen = init_instagram_post()
+        textbox = ShopNameTextbox((500, 100), (20, 0), shop_name) 
+        logo = Logo((100, 100), (225, 100)) 
+        image1 = Image1((75, 75),(85, 250))
+        price1 = PriceTextbox((75, 50), (85, 350), price1_input)
+        image2 = Image2((75, 75),(235, 250))
+        price2 = PriceTextbox((75, 50), (235, 350), price2_input)
+        image3 = Image3((75, 75),(385, 250)) 
+        price3 = PriceTextbox((75, 50), (385, 350), price3_input)
     elif size_choice == "3":
         screen = init_webpage_size()
         textbox = Textbox()
@@ -193,8 +229,11 @@ def main():
         textbox.draw(screen)
         logo.draw(screen)
         image1.draw(screen)
+        price1.draw(screen)
         image2.draw(screen)
+        price2.draw(screen)
         image3.draw(screen)
+        price3.draw(screen)
         # update the display for this frame
         pygame.display.flip()
 
